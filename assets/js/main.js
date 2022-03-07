3c9e8c69e23eee58496daa20e7a1d16517bb1461
Ayan <ayanr143@gmail.com>
2022-03-07 19:30:28 +0530

    f1

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+
+!(function($) {
+  "use strict";
+
+ 
+
+  // Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      900: {
+        items: 3
+      }
+    }
+  });
+
+
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out-back",
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+
+})(jQuery);
